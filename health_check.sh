486482b74fec32f12269f6f532893f158a034327
abhibabar007 <abhijitbabar7@yahoo.com>
2023-10-25 11:44:23 +0530

    Server health Check - 1

---
+++ health_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Owner : Abhijit Babar
+# Date  : 1 Aug 2023
+
+HOSTNAME=`hostname`
+DATETIME=`date "+%Y-%m-%d-%H:%M:%S"`
+CPUUSAGE=`top -b -n 1 -d1 | grep "Cpu(s)" | awk '{print $2}'`
+MEMUSAGE=`free | grep Mem | awk '{print $3/$2*100.0}'`
+DISKUSAGE=`df -h | column -t | awk '{print $5}' | tail -3 | head -1`
+
+
+echo 'Hostname Date-Time CPU-Usage% Mem-Usage% Disk-Usage%'
+echo "$HOSTNAME $DATETIME $CPUUSAGE $MEMUSAGE $DISKUSAGE"
